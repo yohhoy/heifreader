51c978e6717b854e3d503d0a460a6a2cc5c0bec9
yoh <kawasaki.liamg@gmail.com>
2017-08-09 14:45:55 +0900

    parse ItemPropertyAssociation payload

--- app/src/main/java/jp/yohhoy/heifreader/iso14496/part12/ItemPropertyAssociation.java
+++ app/src/main/java/jp/yohhoy/heifreader/iso14496/part12/ItemPropertyAssociation.java
@@ -4,6 +4,8 @@ import org.mp4parser.support.AbstractFullBox;
 import org.mp4parser.tools.IsoTypeReader;
 
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
 
 /*
  * <h1>4cc = "{@value #TYPE}"</h1>
@@ -12,9 +14,24 @@ public class ItemPropertyAssociation extends AbstractFullBox {
     public static final String TYPE = "ipma";
 
     private long contentSize;
+    private List<Item> items = new ArrayList<>();
 
     public ItemPropertyAssociation() {
         super(TYPE);
+    }
+
+    static public class Assoc {
+        public int essential;
+        public int property_index;
+    }
+
+    static public class Item {
+        public long item_ID;
+        public List<Assoc> associations = new ArrayList<>();
+    }
+
+    public List<Item> getItems() {
+        return items;
     }
 
     @Override
@@ -24,21 +41,28 @@ public class ItemPropertyAssociation extends AbstractFullBox {
         parseVersionAndFlags(content);
         long entry_count = IsoTypeReader.readUInt32(content);
         for (long i = 0; i < entry_count; i++) {
-            long item_ID;
+            Item item = new Item();
             if (getVersion() < 1) {
-                item_ID = IsoTypeReader.readUInt16(content);
+                item.item_ID = IsoTypeReader.readUInt16(content);
             } else {
-                item_ID = IsoTypeReader.readUInt32(content);
+                item.item_ID = IsoTypeReader.readUInt32(content);
             }
             int association_count = IsoTypeReader.readUInt8(content);
             for (int j = 0; j < association_count; j++) {
-                int essential_property_index;
+                Assoc assoc = new Assoc();
+                int value, indexLength;
                 if ((getFlags() & 1) == 1) {
-                    essential_property_index = IsoTypeReader.readUInt16(content);
+                    value = IsoTypeReader.readUInt16(content);
+                    indexLength = 15;
                 } else {
-                    essential_property_index = IsoTypeReader.readUInt8(content);
+                    value = IsoTypeReader.readUInt8(content);
+                    indexLength = 7;
                 }
+                assoc.essential = value >> indexLength;
+                assoc.property_index = value & ((1 << indexLength) - 1);
+                item.associations.add(assoc);
             }
+            items.add(item);
         }
     }
 
